9a0b2ecef8d7e8967d390b69421812f88bb0bed4
Maksim Kim <imakskim@gmail.com>
2018-08-02 00:43:39 +0300

    modular-config

--- webpack.config.js
+++ webpack.config.js
@@ -2,44 +2,48 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const pug = require('./webpack/pug');
+const devserver = require('./webpack/devserver');
 
 // additional plugins
 
 // module settings
-module.exports = {
-  // базовый путь к проекту
-  context: path.resolve(__dirname, 'src'),
+const common = merge ([
+  {
+    // базовый путь к проекту
+    context: path.resolve(__dirname, 'src'),
 
-  // точки входа JS
-  entry: {
-    // основной файл приложения
-    app: [
-      './js/app.js'
-    ],
-  },
+    // точки входа JS
+    entry: {
+      // основной файл приложения
+      app: [
+        './js/app.js'
+      ],
+    },
 
-  //путь для собранных файлов
-  output: {
-    filename: 'js/[name].js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.pug'
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.pug$/,
-        loader: 'pug-loader',
-        options: {
-          pretty: true
-        }
-      }
+    //путь для собранных файлов
+    output: {
+      filename: 'js/[name].js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.pug'
+      })
     ]
   },
-  devServer: {
-    stats: "errors-only"
+    pug()
+]);
+
+module.exports = function(env) {
+  if (env === 'production'){
+    return common;
   }
-};
+  if (env === 'development'){
+    return merge([
+      common,
+      devserver()
+    ])
+  }
+}
